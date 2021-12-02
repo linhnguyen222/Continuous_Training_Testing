bf69d4bccda5b43611cd0de2b60faa2273f0e5b0
Nguyen Linh <nguyenlinh@Nguyens-MBP.local>
2021-12-02 21:20:18 +0200

    remove redundant folders

--- MLService_rpi/dags/run_streaming.py
+++
@@ -1,8 +0,0 @@
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
-from data_streaming.data_streaming import *
-print("streaming")
-handle_streaming_data()
-
